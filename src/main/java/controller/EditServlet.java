ce1c4d2af77bcbd966f0c1c74be432918ec03b9e
keita_nakazawa <keita.nakazawa@excite-software.net>
2021-09-28 18:01:31 +0900

    EditServlet開始

--- src/main/java/controller/EditServlet.java
+++ src/main/java/controller/EditServlet.java
@@ -7,11 +7,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Memo;
 
 @WebServlet("/EditServlet")
 public class EditServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String studentNumber = request.getParameter("studentNumber");
+		String studentName = request.getParameter("studentName");
+		String memoText = request.getParameter("memo");
 		
+		HttpSession session = request.getSession();
+		Memo memo = (Memo)session.getAttribute("memo");
+		
+		
+		
+		
+		if(memoText.equals(memo.getMemo()) == false) {
+			//メモ作成者更新処理
+			
+			
+			//memoTextが無記入、かつmemoがnullの場合はtrue? false?
+		}
 	}
 
 }
